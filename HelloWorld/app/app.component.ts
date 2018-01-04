110422fd0f9c3e8daedb157126f2fef626f68f8c
Phodal HUANG <h@phodal.com>
2018-01-04 13:29:21 +0800

    [T] add basic webview

--- HelloWorld/app/app.component.ts
+++ HelloWorld/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+import * as webViewModule from "tns-core-modules/ui/web-view";
 
 @Component({
-  selector: "my-app",
-  template: `
-    <ActionBar title="My App" class="action-bar"></ActionBar>
-    <!-- Your UI components go here -->
-  `
+    selector: "my-app",
+    template: `<Page>
+        <WebView src="https://www.phodal.com"></WebView>
+    </Page>`
 })
+
 export class AppComponent {
-  // Your TypeScript logic goes here
+    ngOnInit() {
+        let webView = new webViewModule.WebView();
+        webView.on(webViewModule.WebView.loadFinishedEvent, function (args: webViewModule.LoadEventData) {
+            let message;
+            if (!args.error) {
+                message = "WebView finished loading " + args.url;
+            }
+            else {
+                message = "Error loading " + args.url + ": " + args.error;
+            }
+
+        });
+        webView.src = "https://www.phodal.com/";
+    }
 }
